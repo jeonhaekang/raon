828bfc7fee5935e0a05bac48f4626e554a5a928d
jeonhaekang <1003gorkd@naver.com>
2024-03-01 19:22:44 +0900

    -

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,3 +1,10 @@
+import clsx from "clsx";
+import Image from "next/image";
+
 export const Footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer className={clsx("flex flex-col items-center py-9 px-6 sm:px-12 bg-[#d2d2d2]")}>
+      <Image src="/brandLogo.svg" width={232} height={50} alt="brand logo" />
+    </footer>
+  );
 };
