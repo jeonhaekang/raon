2188cefccd6a9b2922c819119c4fa2e35004c768
jeonhaekang <1003gorkd@naver.com>
2024-03-01 19:13:20 +0900

    -

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { Button } from "@/components/common";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +10,37 @@ export default function Home() {
       <section className="w-full">
         <div className="relative w-full pt-[125%] sm:pt-[75%]">
           <Image src="/main.jpg" alt="main image" fill={true} className="object-cover" />
+
+          <div className="absolute overflow-hidden top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white">
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.7 }}
+              className="text-6xl font-extralight"
+            >
+              WELLNESS
+              <br />
+              DESIGN
+            </motion.p>
+
+            <motion.p
+              initial={{ opacity: 0, translateY: "100%" }}
+              animate={{ opacity: 1, translateY: "0" }}
+              transition={{ delay: 0.5, duration: 0.5 }}
+              className="font-medium"
+            >
+              世界を、笑顔に。
+            </motion.p>
+          </div>
+
+          <motion.a
+            initial={{ opacity: 0, translateX: "-50%", translateY: "100%" }}
+            animate={{ opacity: 1, translateY: "0" }}
+            transition={{ delay: 0.6, duration: 0.8, ease: "easeOut" }}
+            className="absolute bottom-10 left-1/2"
+          >
+            <Button width="fix" title="ABOUT US" description="ラオンについて" />
+          </motion.a>
         </div>
       </section>
 
@@ -17,11 +51,6 @@ export default function Home() {
           遊び心のあるデザインも、カラダや地球にやさしい素材を選ぶことも、すべては
           “人々に心身ともに健康で、笑顔あふれる毎日を届けたい”という願いから。
         </p>
-      </section>
-
-      <section className="bg-gray-500 w-full flex flex-col justify-center items-center p-24 gap-4">
-        <Button color="black" title="MAGAZINE" description="MASH GROUP マガジン" />
-        <Button size="fix" title="MAGAZINE" description="MASH GROUP マガジン" />
       </section>
     </div>
   );
--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,26 +1,30 @@
 import clsx from "clsx";
 import { ButtonHTMLAttributes } from "react";
 
-const sizeClass = {
+const widthClass = {
   fix: "min-w-[350px]",
   full: "w-full",
 } as const;
 
 const colorClass = {
-  black: "border-black text-black",
-  white: "border-white text-white",
+  black: "border-black text-black hover:text-white hover:bg-black",
+  white: "border-white text-white hover:text-black hover:bg-white",
 };
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   title?: string;
   description?: string;
-  size?: keyof typeof sizeClass;
+  width?: keyof typeof widthClass;
   color?: keyof typeof colorClass;
 }
 
-export const Button = ({ title, description, size = "full", color = "white" }: ButtonProps) => {
+export const Button = ({ title, description, width = "full", color = "white" }: ButtonProps) => {
   return (
-    <button className={clsx(`flex flex-col items-center p-[18px] border ${colorClass[color]} ${sizeClass[size]}`)}>
+    <button
+      className={clsx(
+        `flex flex-col items-center p-[12px] duration-1000 border ${colorClass[color]} ${widthClass[width]}`
+      )}
+    >
       <p className="text-4xl font-thin">{title}</p>
 
       {description && <p className="text-lg">{description}</p>}
