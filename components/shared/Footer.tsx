1fa1cf3b5a152185e4d9fd4dee2de8f9913f18aa
jeonhaekang <1003gorkd@naver.com>
2024-03-02 10:43:39 +0900

    -

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -4,20 +4,23 @@ import clsx from "clsx";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { useGlobalNav } from ".";
+import { InstagramIcon } from "../common";
 
 export const Footer = () => {
   const { toggle, control } = useGlobalNav();
 
   return (
     <motion.footer className={clsx("flex flex-col items-center py-9 px-6 sm:px-12 bg-[#d2d2d2]")} {...control}>
-      <Image src="/brandLogo.svg" width={232} height={50} alt="brand logo" />
+      <Image src="/logo.svg" width={232} height={50} alt="brand logo" />
 
       <div className="border-t my-6 w-full border-black" />
 
       <div className="flex flex-col gap-2 items-center">
         <p className="text-[10px]">OFFICIAL SNS</p>
 
-        <Image src="/icons/instagram.svg" alt="instagram" width={24} height={24} />
+        <a href="#" className="text-gray-800">
+          <InstagramIcon />
+        </a>
       </div>
 
       <p className="text-[9px] mt-4">© LAON Co.,Ltd.</p>
